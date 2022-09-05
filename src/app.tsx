d8c0795985707eb0cc40ffa309373770b9656be7
Ariane Brandao <ariplaymad@gmail.com>
2022-09-05 17:48:33 -0300

    feat: sidebar component done

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,25 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+
+import styles from "./app.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Ariane Brandão"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem aliquid reiciendis animi, quis at modi inventore facilis repudiandae distinctio amet exercitationem dolores assumenda! Harum tempore veniam natus? Voluptatum, praesentium nobis."
+          />
+          <Post author="Aleksandar" content="Aleksandar" />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Post/index.tsx
@@ -0,0 +1,13 @@
+interface PostProps {
+    author: string;
+    content: string;
+}
+
+export function Post(props: PostProps) {
+    return (
+        <div>
+            <h3>{props.author}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+}
